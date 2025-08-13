280b78f768f49c00254b7069b3e9ac065ab46c33
Guilherme <guilherme.sanaiotti@hotmail.com>
2025-08-12 21:52:26 -0300

    Classe Product atualizada, definindo os dados e seus tipos

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    nome_loja = models.CharField(max_length=30)
+    nome_produto = models.CharField(max_length=100)
+    preco_produto = models.DecimalField(max_digits=10, decimal_places=2)
+    url_produto = models.URLField()
+    url_foto = models.URLField()
+    status_ativo = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nome_produto} - {self.nome_loja}"
